1118fe5a00bdf58011ad057f6983848dead0a100
Ignacio Boghossian <boghossianignacio@gmail.com>
2023-05-29 00:01:14 -0300

    cambios y funcionalidad

--- app.js
+++ app.js
@@ -1,12 +1,79 @@
-document.querySelector(".bars__menu").addEventListener("click", animateBars);
+const contenedorProductos = document.getElementById('contenedorProductos');
 
-var line1__bars = document.querySelector(".line1__bars-menu");
-var line2__bars = document.querySelector(".line2__bars-menu");
-var line3__bars = document.querySelector(".line3__bars-menu");
+const contenedorCarrito = document.getElementById('carrito-contenedor')
 
-function animateBars(){
-    line1__bars.classList.toggle("activeline1__bars-menu");
-    line2__bars.classList.toggle("activeline2__bars-menu");
-    line3__bars.classList.toggle("activeline3__bars-menu");
+const botonVaciar = document.getElementById('vaciar-carrito')
+
+const contadorCarrito = document.getElementById('contadorCarrito')
+
+const cantidad = document.getElementById('cantidad')
+
+const precioTotal = document.getElementById('precioTotal')
+
+let carrito = []
+
+
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0
+    actualizarCarrito()
+})
+    
+
+stockProductos.forEach((producto) =>{
+    const div = document.createElement('div')
+    div.classList.add('producto')
+    div.innerHTML = ` 
+    <img src=${producto.img} alt= ""> 
+    <h3>${producto.nombre}</h3>
+    <p>${producto.descripcion}</p>
+    <p class="precioProducto">Precio: <span class="orangeColor fontBold"> ${producto.precio} ETH</span></p>
+    <button id="agregar${producto.id}" class="botonAgregar">Agregar <i class="fas fa-cart-shopping hoverOrange"></i></button>       
+    `
+
+    contenedorProductos.appendChild(div);
+    
+    const boton = document.getElementById(`agregar${producto.id}`)
+
+    boton.addEventListener('click', () => {
+        agregarAlCarrito(producto.id)
+    })      
+        
+   })
+
+   const agregarAlCarrito = (prodId) => {
+    const item = stockProductos.find((prod) => prod.id === prodId)
+    carrito.push(item)
+    actualizarCarrito()
+    console.log(carrito) 
 }
 
+const eliminarDelCarrito = (prodId) =>{
+    const item = carrito.find((prod) => prod.id === prodId)
+    const indice = carrito.indexOf(item)
+    carrito.splice(indice, 1)
+    actualizarCarrito()
+}
+
+
+const actualizarCarrito = () => {
+    contenedorCarrito.innerHTML = ""
+
+    carrito.forEach((prod) =>{
+        const div= document.createElement('div')
+        div.className=('productoEnCarrito')
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio: ${prod.precio} ETH</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
+
+        contenedorCarrito.appendChild(div)
+        
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+    })
+
+    contadorCarrito.innerText = carrito.length
+
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
+}
---
+++ stock/stock.js
@@ -0,0 +1,19 @@
+const stockProductos = [
+{id: 1, nombre: "Bored Ape Yacht #0001", cantidad: 1, descripcion:"Bored Ape Yacht", precio: 40,  img: '../stock/stockImg/mono1.jpg'},
+{id: 2, nombre: "Bored Ape Yacht #0002", cantidad: 1, descripcion:"Bored Ape Yacht", precio: 35, img: '../stock/stockImg/mono3.jpg'},
+{id: 3, nombre: "Bored Ape Yacht #0003", cantidad: 1, descripcion:"Bored Ape Yacht", precio: 30, img: '../stock/stockImg/mono2.jpg'},
+{id: 4, nombre: "Bored Ape Yacht #0004", cantidad: 1, descripcion:"Bored Ape Yacht", precio: 55, img: '../stock/stockImg/mono4.jpg'},
+{id: 5, nombre: "Bored Ape Yacht #0005", cantidad: 1, descripcion:"Bored Ape Yacht", precio: 90, img: '../stock/stockImg/mono5.jpg'},
+{id: 6, nombre: "Bored Ape Yacht #0006", cantidad: 1, descripcion:"Bored Ape Yacht", precio: 10, img: '../stock/stockImg/mono6.jpg'},
+{id: 7, nombre: "Bored Ape Yacht #0007", cantidad: 1, descripcion:"Bored Ape Yacht", precio: 5, img: '../stock/stockImg/mono7.jpg'},
+{id: 8, nombre: "Bored Ape Yacht #0008", cantidad: 1, descripcion:"Bored Ape Yacht", precio: 15, img: '../stock/stockImg/mono9.jpg'},
+{id: 9, nombre: "Bored Ape Yacht #0009", cantidad: 1, descripcion:"Bored Ape Yacht", precio: 45, img: '../stock/stockImg/mono8.jpg'},
+{id: 10, nombre: "Bored Ape Yacht #0010", cantidad: 1, descripcion:"Bored Ape Yacht", precio: 25, img: '../stock/stockImg/mono10.jpg'},
+{id: 11, nombre: "Bored Ape Yacht #0011", cantidad: 1, descripcion:"Bored Ape Yacht", precio: 10, img: '../stock/stockImg/mono11.jpg'},
+{id: 12, nombre: "Bored Ape Yacht #0012", cantidad: 1, descripcion:"Bored Ape Yacht", precio: 5, img: '../stock/stockImg/mono12.jpg'},
+{id: 13, nombre: "Bored Ape Yacht #0013", cantidad: 1, descripcion:"Bored Ape Yacht", precio: 10, img: '../stock/stockImg/mono13.jpg'},
+{id: 14, nombre: "Bored Ape Yacht #0014", cantidad: 1, descripcion:"Bored Ape Yacht", precio: 3, img: '../stock/stockImg/mono14.jpg'},
+{id: 15, nombre: "Bored Ape Yacht #0015", cantidad: 1, descripcion:"Bored Ape Yacht", precio: 4, img: '../stock/stockImg/mono15.jpg'},
+
+
+]
